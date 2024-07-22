568aaf3c90fbd59ba555c7b18abf675427f5cfe4
Sakda-Phoda <mr.sakda.phoda@gmail.com>
2024-07-22 18:34:12 +0700

    Update the query object.

--- test_2.sql
+++ test_2.sql
@@ -1,19 +1,24 @@
+-- Query 2: Find the number of transactions, total sales, and average sales. Also, indicate whether the sales performance was above or below average by grouping by month.
+
 -- Common Table Expression (CTE) to calculate total sales and number of transactions per month
-WITH monthly_sales AS (
-    SELECT 
-        strftime('%Y-%m', transaction_date) AS Month,
-        COUNT(transaction_id) AS Number_of_Transaction,
-		SUM(total_amount) AS Total_Sale
-    FROM trans
-    GROUP BY Month
-),
+WITH
+monthly_sales AS 
+	(
+	    SELECT 
+	        strftime('%Y-%m', transaction_date) AS Month,
+	        COUNT(transaction_id) AS Number_of_Transaction,
+			SUM(total_amount) AS Total_Sale
+	    FROM trans
+	    GROUP BY Month
+	),
 
 -- CTE to calculate the average total sales per month
-average_sales AS (
-    SELECT 
-        SUM(total_amount) / COUNT(DISTINCT strftime('%Y-%m', transaction_date)) AS Average_Total_Sale
-    FROM trans
-)
+average_sales AS 
+	(
+	    SELECT 
+	        SUM(total_amount) / COUNT(DISTINCT strftime('%Y-%m', transaction_date)) AS Average_Total_Sale
+	    FROM trans
+	)
 
 -- Main query to join the CTEs and calculate the sales performance
 SELECT 
